097d9ef5660f6d4cc59ef6d2b5405d0e7867f40c
Cole-Hayden <cwh8b4@gmail.com>
2020-05-14 10:38:38 -0500

    Updates

---
+++ precache-manifest.6dbf6cccaef1b7e2efded17aaca58aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bc9812e49aebccfd6b9b4f603d9b08",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/index.html"
+  },
+  {
+    "revision": "3fc753eb3a347fbe8012",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d104612467ceb179f33",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/js/2.a2ed73ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/js/2.a2ed73ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc753eb3a347fbe8012",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/js/main.9a53e42c.chunk.js"
+  },
+  {
+    "revision": "04a8075ed06a4a99cd35",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/js/runtime-main.5f152ebb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cole-Hayden/ChicagoRoommatesClient.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
